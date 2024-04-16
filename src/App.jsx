2a25f3e82841d5b721e18ff63de9f88d2520280b
Samir Mulla <samirmulla710@gmail.com>
2024-04-16 06:45:54 +0530

    refactor : App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
 import Widgets from "./components/Widgets";
-import Register from "./components/Register";
+// import Register from "./components/Register";
 
 function App() {
-  const [showRegister, setShowRegister] = useState(true);
-  const [showComponents, setShowComponents] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowRegister(false);
-      setShowComponents(true);
-    }, 3000); // Change this value to adjust the delay
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <div className="App">
       {/* Header */}
-      {showComponents && <Header />}
+      <Header />
       {/* App body */}
       <div className="app__body">
         <div className="md:flex justify-between mt-10 mx-4 ">
           {/* Sidebar */}
-          {showComponents && <Sidebar />}
+          <Sidebar />
 
           {/* Feed */}
-          {showComponents && <Feed />}
+          <Feed />
 
           {/* Widgets */}
-          {showComponents && <Widgets />}
-
-          {showRegister && <Register />}
+          <Widgets />
         </div>
       </div>
     </div>
